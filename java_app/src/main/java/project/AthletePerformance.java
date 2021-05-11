06ed7cce15350479a5097a1832ea1e74557e54c4
Grigoris <greg.bouzios@gmail.com>
2021-05-12 00:54:10 +0300

    Added class for first task

--- java_app/src/main/java/project/AthletePerformance.java
+++ java_app/src/main/java/project/AthletePerformance.java
@@ -1,8 +1,10 @@
 package project;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.StringReader;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -15,23 +17,38 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AthletePerformance {
 
-    public static class MedalMapper
+    public static class GoldMedalMapper
             extends Mapper<Object, Text, Text, IntWritable>{
+
         private final static IntWritable one = new IntWritable(1);
+        private final Text athleteMeta = new Text();
+        private final Text medal = new Text();
+        private final static Text gold = new Text("Gold");
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
+            String line = value.toString();
+
+            try (CSVReader csvReader = new CSVReader(new StringReader(line))) {
+                String[] ParsedLine = csvReader.readNext();
+                String meta = String.format("%s %s %s",
+                        ParsedLine[Constants.ID],
+                        ParsedLine[Constants.NAME],
+                        ParsedLine[Constants.SEX]);
+                medal.set(ParsedLine[Constants.MEDAL]);
+                athleteMeta.set(meta);
+            } catch (CsvValidationException e) {
+                e.printStackTrace();
+            }
+            if (medal.equals(gold)) {
+                context.write(athleteMeta, one);
             }
         }
     }
 
-    public static class AthleteReducer
+    public static class GoldMedalReducer
             extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
+        private final IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values,
                            Context context
@@ -49,9 +66,9 @@ public class AthletePerformance {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "athlete performance");
         job.setJarByClass(AthletePerformance.class);
-        job.setMapperClass(MedalMapper.class);
-        job.setCombinerClass(AthleteReducer.class);
-        job.setReducerClass(AthleteReducer.class);
+        job.setMapperClass(GoldMedalMapper.class);
+        job.setCombinerClass(GoldMedalReducer.class);
+        job.setReducerClass(GoldMedalReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
